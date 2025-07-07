cf6046eb2fc2daf16150916a06c2230e7430435e
requinpham <kyanhpham0403@gmail.com>
2025-07-07 10:16:25 +0700

    hold code

--- TrackMentalHealth_client/src/components/loginPage/LoginForm.jsx
+++ TrackMentalHealth_client/src/components/loginPage/LoginForm.jsx
@@ -53,7 +53,7 @@ const LoginForm = () => {
 
                 if (decoded.roles && decoded.roles.includes('ROLE_ADMIN')) {
                     navigate('/dashboard');
-                } else if (decoded.roleId.id === 1) {
+                } else if (decoded.roles && decoded.roles.includes('ROLE_USER')) {
                     navigate('/user/homepage');
                 }
             } catch (err) {
@@ -94,81 +94,6 @@ const LoginForm = () => {
 
 
     return (
-        // <form onSubmit={formik.handleSubmit}>
-        //     <Stack>
-        //         <Box>
-        //             <Typography variant="subtitle1" fontWeight={600} htmlFor='email' mb="5px">Email</Typography>
-        //             <CustomTextField
-        //                 id="email"
-        //                 name="email"
-        //                 value={formik.values.email}
-        //                 onChange={formik.handleChange}
-        //                 onBlur={formik.handleBlur}
-        //                 error={formik.touched.email && Boolean(formik.errors.email)}
-        //                 helperText={formik.touched.email && formik.errors.email}
-        //                 variant="outlined"
-        //                 fullWidth
-        //             />
-        //         </Box>
-        //         <Box mt="25px">
-        //             <Typography variant="subtitle1" fontWeight={600} htmlFor='password' mb="5px">Password</Typography>
-        //             <CustomTextField
-        //                 id="password"
-        //                 name="password"
-        //                 type="password"
-        //                 value={formik.values.password}
-        //                 onChange={formik.handleChange}
-        //                 onBlur={formik.handleBlur}
-        //                 error={formik.touched.password && Boolean(formik.errors.password)}
-        //                 helperText={formik.touched.password && formik.errors.password}
-        //                 variant="outlined"
-        //                 fullWidth
-        //             />
-        //         </Box>
-        //         <Stack justifyContent="space-between" direction="row" alignItems="center" my={2}>
-        //             <FormGroup>
-        //                 <FormControlLabel
-        //                     control={
-        //                         <Checkbox
-        //                             checked={formik.values.remember}
-        //                             onChange={formik.handleChange}
-        //                             name="remember"
-        //                         />
-        //                     }
-        //                     label="Remember this Device"
-        //                 />
-        //             </FormGroup>
-        //             <Typography
-        //                 component="button"
-        //                 onClick={() => navigate('/forgot-password')}
-        //                 fontWeight="500"
-        //                 sx={{
-        //                     textDecoration: 'none',
-        //                     color: 'primary.main',
-        //                     background: 'none',
-        //                     border: 'none',
-        //                     cursor: 'pointer',
-        //                     padding: 0,
-        //                     fontSize: 'inherit',
-        //                     fontFamily: 'inherit'
-        //                 }}
-        //             >
-        //                 Forgot Password ?
-        //             </Typography>
-
-        //         </Stack>
-        //     </Stack>
-        //     {errorMessage && (
-        //         <Alert severity="error" sx={{ mb: 2 }}>
-        //             {errorMessage}
-        //         </Alert>
-        //     )}
-        //     <Box>
-        //         <Button color="primary" variant="contained" size="large" fullWidth type="submit">
-        //             Sign In
-        //         </Button>
-        //     </Box>
-        // </form>
         <AuthLogin
             subtitle={
                 <Stack direction="row" spacing={1} justifyContent="center" mt={3}>
